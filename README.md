ad40351046257a52b65d999b6f93be44c5c697a7
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-02-04 22:35:04 +1000

    Added solution and build configuration.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Autofac.Extras.NHibernate
-Autofac implementation of the NHibernate factories
+Autofac implementation of the NHibernate factories that allow for dependency injection into entities and NHibernate objects.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/vpkf5dlbw5ehfeng?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-nhibernate)
--- src/Autofac.Extras.NHibernate/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.NHibernate/Properties/AssemblyInfo.cs
@@ -1,7 +1,23 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyTitle("Autofac.Extras.NHibernate")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Autofac implementation of the NHibernate factories")]
