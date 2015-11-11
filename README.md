53b6d704c6cfb61a6196658a0850190417726e43
Travis Illig <tillig@paraesthesia.com>
2015-11-11 07:33:03 -0800

    Updated to .NET 4.5 and VS 2015.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Autofac.Extras.NHibernate
-Autofac implementation of the NHibernate factories that allow for dependency injection into entities and NHibernate objects.
+[Autofac](http://autofac.org) implementation of the NHibernate factories that allow for dependency injection into entities and NHibernate objects.
 
 [![Build status](https://ci.appveyor.com/api/projects/status/vpkf5dlbw5ehfeng?svg=true)](https://ci.appveyor.com/project/Autofac/autofac-extras-nhibernate)
+
+**MAINTENANCE MODE**: This package is in maintenance-only mode. Bug fixes may be addressed and Autofac compatibility may be checked but no new features will be added.
+
+Please file issues and pull requests for this package in this repository rather than in the Autofac core repo.
+
+- [NuGet](https://www.nuget.org/packages/Autofac.Extras.NHibernate/)
+- [Contributing](http://autofac.readthedocs.org/en/latest/contributors.html)
--- src/Autofac.Extras.NHibernate/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.NHibernate/Properties/AssemblyInfo.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 [assembly: AssemblyTitle("Autofac.Extras.NHibernate")]
-[assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-[assembly: AllowPartiallyTrustedCallers]
-
-[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
-[assembly: AssemblyProduct("Autofac")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
-[assembly: AssemblyConfiguration("Release")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
-[assembly: AssemblyDescription("Autofac implementation of the NHibernate factories")]
+[assembly: AssemblyDescription("Autofac implementation of NHibernate factories.")]
+
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyInformationalVersion("4.0.0.0")]
